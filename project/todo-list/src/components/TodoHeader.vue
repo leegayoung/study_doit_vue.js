25697c29cd03aae57cfe245547f7a04ec1999df1
leegayoung <leega0711@naver.com>
2020-03-20 23:20:11 +0900

    input components edit & localstorage

--- project/todo-list/src/components/TodoHeader.vue
+++ project/todo-list/src/components/TodoHeader.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>header</div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
 <script>
@@ -7,5 +9,11 @@ export default {
 }
 </script>
 
-<style >
+<!-- scoped 속성은 스타일 정의를 해당 컴포넌트에서만 적용한다는 뜻 -->
+<style scoped>
+  h1{
+    color: #2f3b52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
 </style>
--- project/todo-list/src/components/TodoInput.vue
+++ project/todo-list/src/components/TodoInput.vue
@@ -1,11 +1,61 @@
+
 <template>
-  <div>input</div>
+<div class="inputBox shadow">
+  <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keypress.enter="addTodo">
+  <span class="addContainer" v-on:click="addTodo">
+    <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+  </span>
+</div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods:{
+    addTodo(){
+      if(this.newTodoItem !== ""){
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value,value);
+        this.clearInput();
+      }
+    },
+    clearInput(){
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
-<style >
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox{
+  background: white;
+  height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  line-height: 50px;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: inline-block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+  /* 높이가 깨져서 임의로 추가 */
+  height: 3rem;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+  line-height: 50px;
+}
 </style>
